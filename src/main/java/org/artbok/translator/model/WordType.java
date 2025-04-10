65a88c4177117c4465c700ff0fae89fb56c45363
Temjik <artem.bokans@gmail.com>
2025-04-10 19:42:37 +0300

    + new files

---
+++ src/main/java/org/artbok/translator/model/WordType.java
@@ -0,0 +1,39 @@
+package org.artbok.translator.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wordTypes")
+@Getter
+@Setter
+@Builder
+public class WordType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column
+    public String word;
+
+    @Column
+    public String wordLanguage;
+
+    @Column
+    public String article;
+
+    @Column
+    public String plural;
+
+    @Column
+    public String translation;
+
+    @Column
+    public String translationLanguage;
+
+    @Column
+    public String creator;
+
+}
